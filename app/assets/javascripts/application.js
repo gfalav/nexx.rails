08d20770cf73a49652e24a1baf369cfdfe9563e0
Gustavo Falavigna <gfalav@hotmail.com>
2019-09-13 20:02:25 -0300

    working on direcciones

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery-3.4.1.min
 //= require jquery.easy-autocomplete
 //= require activestorage
