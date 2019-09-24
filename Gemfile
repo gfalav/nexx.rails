ea9037ef8d24ac412bedfabe442fde7cacbde5dc
Gustavo Falavigna <gfalav@hotmail.com>
2019-09-24 18:12:13 -0300

    working on qr code

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'devise'
 gem 'prawn'
-
+gem 'prawn-qrcode'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/pdfs/facturaPdf.rb
+++ app/pdfs/facturaPdf.rb
@@ -1,5 +1,9 @@
 class FacturaPdf < Prawn::Document
+	
+
 	def initialize
+		require 'prawn/qrcode'
+
 		super(page_size: "A4", margin: 0)
 		font_families.update(
 			"OpenSansCond" => {
@@ -17,11 +21,99 @@ class FacturaPdf < Prawn::Document
 		fondo
 		nis
 		apell_nombre
+		nrofactura
+		dompostal
+		vencimiento
+		condfiscal
+		documento
+		iibb
+		perfact
+		femision
+		fupb
+		fproxvcto
+		qrcodigo
 	end
 
-	def fondo
-		image Rails.root.join("app/assets/images/facturaFrente.png"), height: 841
+	def qrcodigo		
+		qrcode_content = "http://github.com/jabbrwcky/prawn-qrcode"
+		qrcode = RQRCode::QRCode.new(qrcode_content)
+		render_qr_code(qrcode)
 	end
+
+	def fproxvcto
+		bounding_box([408,717], :width=>300, :height=>100) do
+			font_size 11
+			text "00/00/0000", :inline_format => true
+		end		
+	end
+
+	def fupb
+		bounding_box([308,717], :width=>300, :height=>100) do
+			font_size 11
+			text "00/00/0000", :inline_format => true
+		end		
+	end
+
+	def femision
+		bounding_box([222,717], :width=>300, :height=>100) do
+			font_size 11
+			text "00/00/0000", :inline_format => true
+		end		
+	end
+
+	def perfact
+		bounding_box([150,717], :width=>300, :height=>100) do
+			font_size 11
+			text "00/0000", :inline_format => true
+		end		
+	end
+
+	def iibb
+		bounding_box([402,743], :width=>300, :height=>100) do
+			font_size 11
+			text "123-18504604", :inline_format => true
+		end		
+	end
+
+	def documento
+		bounding_box([292,743], :width=>300, :height=>100) do
+			font_size 11
+			text "DNI 18504604", :inline_format => true
+		end		
+	end
+
+	def condfiscal
+		bounding_box([135,743], :width=>300, :height=>100) do
+			font_size 11
+			text "Consumidor Final", :inline_format => true
+		end		
+	end
+
+	def vencimiento
+		bounding_box([506,783], :width=>300, :height=>100) do
+			font_size 11
+			text "<b>00/00/0000</b>", :inline_format => true
+		end		
+	end
+
+	def dompostal
+		bounding_box([135,783], :width=>300, :height=>100) do
+			font_size 11
+			text "CL LOS ZORZALES Nro puerta 620 SN Sin Barrio LAS CHACRAS"
+			move_up 3
+			text 'JUANA KOSLAY - CP: 5701', :inline_format => true	
+		end		
+	end
+
+	def nrofactura
+		bounding_box([492,818], :width=>100, :height=>100) do
+			font_size 10
+			text 'Liq Serv Pub "B"(18)', :inline_format => true
+			move_up 3
+			text 'N° 0000-00000000', :inline_format => true	
+		end
+	end
+
 
 	def nis
 		bounding_box([416,814], :width=>100, :height=>100) do
@@ -37,4 +129,7 @@ class FacturaPdf < Prawn::Document
 		end		
 	end
 
+	def fondo
+		image Rails.root.join("app/assets/images/facturaFrente.png"), height: 841
+	end
 end
